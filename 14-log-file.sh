52f6c2fa3ba92a60ca64d59c9f5b954f773da83e
Kiran <kirankm95@gmail.com>
2023-12-13 15:02:40 +0530

    update in shell scripts

---
+++ 14-log-file.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ID=$(id -u)
+DATE=$(date +F%-%H-%M-%S)
+LOGFILE="/tmp/$0-$DATE"
+
+MYFUNCTION (){
+if [ $1 -ne 0 ]
+then
+    echo "Installing $2 package failed"
+    exit 1
+else
+    echo "installing $2 package is SUCCESS buddy"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "You are not root user, sorry"
+    exit 1
+else
+    echo "You are root user, YEAH"
+fi
+
+yum install -y mysql &>> LOGFILE
+MYFUNCTION $? "mysql"
+
+yum install -y git &>> LOGFILE
+MYFUNCTION $? "GIT"
