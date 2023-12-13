f33a1e1aaff1f01773970a7c595b3a922a955943
Kiran <kirankm95@gmail.com>
2023-12-13 15:20:58 +0530

    update in shell scripts

---
+++ 16-colors-1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ID=$(id -u)
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$DATE"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+MYFUNCTION (){
+if [ $1 -ne 0 ]
+then
+    echo -e "Installing $2 package $R failed $N"
+    exit 1
+else
+    echo -e "installing $2 package is $Y SUCCESS $G buddy"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "You are not root user, sorry"
+    exit 1
+else
+    echo "You are root user, YEAH"
+fi
+
+yum install -y mysql &>> $LOGFILE
+MYFUNCTION $? "mysql"
+
+yum install -y git &>> $LOGFILE
+MYFUNCTION $? "GIT"
