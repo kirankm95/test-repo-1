362954a104c47ae356c51c8ee9227e1810b98ed8
Kiran <kirankm95@gmail.com>
2023-12-13 12:14:35 +0530

    update in shell scripts

--- 8-arrays.sh
+++ 8-arrays.sh
@@ -4,3 +4,5 @@ myarray=("kiran" "kumar" "nayanshi")
 echo "first array is ${myarray[0]}"
 echo "second array is ${myarray[1]}"
 echo "third array is ${myarray[2]}"
+
+echo "all arrays is ${myarray[@]}"
