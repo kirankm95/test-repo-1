389a9fa7ebc8784ca3b2694eef3e88a3008c32a6
Kiran <kirankm95@gmail.com>
2023-12-13 13:38:15 +0530

    update in shell scripts

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -4,8 +4,17 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "You are not root user, sorry"
-    #exit 1
+    exit 1
 else
     echo "You are root user, YEAH"
 fi
-echo "Continuing"
+
+yum install -y mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Installing mysql failed"
+    exit 1
+else
+    echo "installing mysql SUCCESS"
+fi
