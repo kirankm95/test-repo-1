1136125c8bd2bc0dd9b9a8c85668641a91710ad5
Kiran <kirankm95@gmail.com>
2023-12-13 13:23:53 +0530

    update in shell scripts

--- 10-install-package.sh
+++ 10-install-package.sh
@@ -5,6 +5,13 @@ userid=`id -u`
 if [ $userid -eq 0 ]
 then 
     yum install -y mysql
+    yum list installed mysql 
+    if [ $? -eq 0 ]
+    then 
+    echo "package install ayndhoroy"
+    else
+    echo "install avvaledhu ra babu"
+    fi
 else
     echo "pora non-root ga"
 fi
