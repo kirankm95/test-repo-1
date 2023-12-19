1bca93f83461ca45b6f4b03ede8ba216c19e3bfc
Kiran <kirankm95@gmail.com>
2023-12-19 16:21:14 +0530

    extra script adding

--- 23-disk-usage.sh
+++ 23-disk-usage.sh
@@ -6,8 +6,8 @@ messege=""
 while IFS= read -r line
 do
 
-usage=$(echo $lline | awk '{print $6F}' | cut -d % f1)
-partition=$(echo $lline | awk '{print $1F}'
+usage=$(echo $line | awk '{print $6F}' | cut -d % f1)
+partition=$(echo $line | awk '{print $1F}')
 
 if [ $usage -gt $DISK_THRESHOLD ]
 then
