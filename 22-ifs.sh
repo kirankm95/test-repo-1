300c6eb28d9371b3186d036f289f8eb5ea2b4a86
Kiran <kirankm95@gmail.com>
2023-12-19 15:50:49 +0530

    extra script adding

--- 22-ifs.sh
+++ 22-ifs.sh
@@ -19,7 +19,7 @@ fi
 while IFS=":" read -r username passwd UID GID comment homefolder shell
 do
     echo "username: $username"
-    echo "password: $passwd
+    echo "password: $passwd"
     echo "UID: $UID"
     echo "GID: $GID"
     echo "comment: $comment"
