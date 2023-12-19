a10cd2f390a296f50008fdf335d4ae49722bf3fa
Kiran <kirankm95@gmail.com>
2023-12-19 15:20:53 +0530

    extra script adding

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -5,7 +5,20 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGFILE="/tmp/$0-$DATE.log"
-ID=$(id -u)
+
+SOURCE="/tmp/logs"
 
 echo "Mama, mana script start ayndhi at $DATE" &>> $LOGFILE
+
+if [ ! -d $SOURCE ]
+then
+    echo "mentioned source directory $SOURCE does not exist"
+fi
+
+DELETE=$(find $SOURCE -type f -name *.log -mtime +14)
+
+while IFS=read -r line 
+do
+echo "Deleting $line"
+done
 
