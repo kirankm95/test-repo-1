76c465f21e296c8353e98dce5a5982c997756de7
Kiran <kirankm95@gmail.com>
2023-12-19 18:09:33 +0530

    extra script adding

--- mail.sh
+++ mail.sh
@@ -6,7 +6,7 @@ BODY=$3
 TO_ADDRESS=$4
 SUBJECT=$5
 
-FINAL_BODY=$(sed -e 's/To_Team/$TO_TEAM/g' -e /s/TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.sh)
+FINAL_BODY=$(sed -e 's/To_Team/$TO_TEAM/g' -e '/s/TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.sh)
 
 echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
 
