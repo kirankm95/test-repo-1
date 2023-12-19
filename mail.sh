19f7ccc4a400c1ba50893446ed6b88b15ac7296f
Kiran <kirankm95@gmail.com>
2023-12-19 18:18:00 +0530

    extra script adding

--- mail.sh
+++ mail.sh
@@ -3,10 +3,11 @@
 TO_TEAM=$1
 ALERT_TYPE=$2
 BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
 
-FINAL_BODY=$(sed -e 's/To_Team/$TO_TEAM/g' -e 's/TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
+FINAL_BODY=$(sed -e 's/To_Team/$TO_TEAM/g' -e 's/TYPE/$ALERT_TYPE/g' -e 's/BODY/$ESCAPE_BODY/g' template.html)
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
 
