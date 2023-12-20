4049338e1158ed1c2db1a01b2464dc41992f5b0c
Kiran <kirankm95@gmail.com>
2023-12-20 15:54:36 +0530

    new shell script

--- 26-aws-ec2-creation.sh
+++ 26-aws-ec2-creation.sh
@@ -6,14 +6,14 @@ INSTANCES=("mongodb" "redis" "mysql" "rebbitmq" "catalogue" "cart" "user" "shipp
 
 for i in "${INSTANCES[@]}"
 do  
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ if $i == "shipping" ]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]")
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "$i: $IP_ADDRESS"
 done
